a4c15d2ba4f9bf1b4859f686df9ebbcdf18c52a2
rmendess <romulobr2005@gmail.com>
2024-10-21 19:23:36 -0300

    header init

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './styles.css'
+
+
+const Header = () => {
+  return (
+    <div>
+      dawdawdawdadwa
+    </div>
+  )
+}
+
+export default Header
---
+++ src/screens/Home/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../../components/Header'
+const Home = () => {
+  return (
+    <div style={{
+width:'100%',
+
+    }}>
+     <Header />
+    </div>
+  )
+}
+
+export default Home
