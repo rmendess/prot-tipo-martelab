4b18f7d100309ebd9eebccaaa0a8433f767726db
rmendess <romulobr2005@gmail.com>
2024-10-21 21:45:06 -0300

    Top header done.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react"
 import Home from  "./screens/Home/"
+import Header from "./components/Header"
+
 const App = () => {
   return (
+    
     <>
    <Home />
     </>
   )
 }
 export default App
+ra
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
-import './styles.css'
-
+import React from "react";
+import "./styles.css";
+import Background from "../../assets/background.jpg";
 
 const Header = () => {
   return (
-    <div>
-      dawdawdawdadwa
-    </div>
-  )
-}
+    <div className="header-container">
+      <div className="header-wave-div">
+        <div class="wave-container">
+          <img src={Background} alt="sem imagem de fundo" />
+        </div>
 
-export default Header
+        <div className="positionUsername">
+          <h1>
+            Bem Vindo Dra.<span className="highlight">Simone</span>
+          </h1>
+        </div>
+      </div>
+    
+    </div>
+  );
+};
+
+export default Header;
